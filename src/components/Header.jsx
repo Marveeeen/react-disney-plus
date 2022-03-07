b6e3ec14205507e34fa5266f55fbf1c33cb3eeb2
Marveeen <jmvroqueee@gmail.com>
2022-03-07 09:28:36 +0800

    Modified components to add redux

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,100 @@
-import React from 'react'
+import React, { useEffect  } from 'react'
 import styled from 'styled-components'
+import { selectUserName, selectUserPhoto, setSignOut, setUserLogin }from '../features/user/userSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import { auth, provider } from "../firebase";
+import { signInWithPopup, signOut } from 'firebase/auth';
+import { useNavigate } from "react-router-dom";
 
 const Header = () => {
-  return (
-    <Nav>
-        <Logo src='/images/logo.svg' alt='logo'></Logo>
-        <NavMenu>
-            <a>
-                <img src='/images/home-icon.svg' alt='home-icon'/>
-                <span>Home</span>
-            </a>
-            <a>
-                <img src='/images/search-icon.svg' alt='home-icon'/>
-                <span>SEARCH</span>
-            </a>
-            <a>
-                <img src='/images/watchlist-icon.svg' alt='home-icon'/>
-                <span>WATCHLIST</span>
-            </a>
-            <a>
-                <img src='/images/original-icon.svg' alt='home-icon'/>
-                <span>ORIGINALS</span>
-            </a>
-            <a>
-                <img src='/images/movie-icon.svg' alt='home-icon'/>
-                <span>Movies</span>
-            </a>
-             <a>
-                <img src='/images/series-icon.svg' alt='home-icon'/>
-                <span>Series</span>
-            </a>
-        </NavMenu>
+    const dispatch = useDispatch();
+    const history = useNavigate();
+    const userName = useSelector(selectUserName)
+    const userPhoto= useSelector(selectUserPhoto)
 
-        <UserImg src='/images/Roque.jpg'/>
-    </Nav>
-  )
+    useEffect(() => {
+        auth.onAuthStateChanged(async (user) => {
+            if(user) {
+                setUser(user);
+                history('/home');
+            }
+        })
+    }, [userName])
+
+    const handleAuth = () => {
+        if(!userName) {
+            signInWithPopup(auth, provider)
+                    .then((result) => {
+                        setUser(result.user)
+                    }).catch((error) => {
+                        alert(error.message)
+                    });
+                   
+        } else if(userName) {
+            signOut(auth).then(() => {
+                dispatch(setSignOut());
+                history('/')
+            }).catch((err) => alert(err.message))
+        }
+    }
+
+    const setUser = (user) => {
+        dispatch(setUserLogin({
+            name: user.displayName,
+            email: user.email,
+            photo: user.photoURL,
+        }))
+    }
+
+
+    return (
+        <Nav>
+            <Logo src='/images/logo.svg' alt='logo'></Logo>
+            {!userName 
+                ? (
+                    <LoginContainer>
+                        <Login onClick={handleAuth}>Login</Login>
+                    </LoginContainer>
+                ) : (
+                    <>
+                         <NavMenu>
+                            <a href='/home'>
+                                <img src='/images/home-icon.svg' alt='home-icon'/>
+                                <span>Home</span>
+                            </a>
+                            <a>
+                                <img src='/images/search-icon.svg' alt='search-icon'/>
+                                <span>SEARCH</span>
+                            </a>
+                            <a>
+                                <img src='/images/watchlist-icon.svg' alt='wacthlist-icon'/>
+                                <span>WATCHLIST</span>
+                            </a>
+                            <a>
+                                <img src='/images/original-icon.svg' alt='original-icon'/>
+                                <span>ORIGINALS</span>
+                            </a>
+                            <a>
+                                <img src='/images/movie-icon.svg' alt='movie-icon'/>
+                                <span>Movies</span>
+                            </a>
+                            <a>
+                                <img src='/images/series-icon.svg' alt='series-icon'/>
+                                <span>Series</span>
+                            </a>
+                        </NavMenu>
+                        <SignOut>
+                            <UserImg src={userPhoto} alt='user'/>
+                            <DropDown>
+                            <span onClick={handleAuth}>Sign out</span>
+                            </DropDown>
+                        </SignOut>
+                    </>
+                )
+            }
+           
+        </Nav>
+    )
 }
 
 export default Header
@@ -45,9 +105,8 @@ const Nav = styled.nav`
     display: flex;
     align-items: center;
     padding: 0 36px;
-    justify-content: space-between;
     overflow-x: hidden
-
+     z-index: 3;
 `
 
 const Logo = styled.img`
@@ -58,11 +117,16 @@ const Logo = styled.img`
 const NavMenu = styled.div`
    display:flex;
    align-items: center;
+   flex: 1;
+   margin-left: 20px;
+
    a {
        display: flex;
        align-items: center;
        padding: 0 12px;
        cursor: pointer;
+       text-decoration: none;
+       color: white;
 
        img {
            height: 20px;
@@ -100,7 +164,72 @@ const NavMenu = styled.div`
 `
 
 const UserImg = styled.img`
+    height: 100%;   
+`
+
+const LoginContainer = styled.div`
+   display: flex;
+   justify-content: flex-end;
+   flex: 1;
+`
+
+const Login = styled.div`
+   border: 1px solid #f9f9f9;
+   padding: 8px 16px;
+   border-radius: 4px;
+   text-transform: uppercase;
+   letter-spacing: 1.5px;
+   background-color: rgba(0, 0, 0, 0.6);
+   cursor: pointer;
+   transition: all 0.2s ease 0s;
+
+   &:hover {
+       background-color: #f9f9f9;
+       color: #000;
+       border-color: transparent;
+   }
+`
+
+const DropDown = styled.div`
+  position: absolute;
+  top: 48px;
+  right: 0px;
+  background: rgb(19, 19, 19);
+  border: 1px solid rgba(151, 151, 151, 0.34);
+  border-radius: 4px;
+  box-shadow: rgb(0 0 0 / 50%) 0px 0px 18px 0px;
+  padding: 5px;
+  text-align: center;
+  font-size: 14px;
+  letter-spacing: 3px;
+  width: 100px;
+  opacity: 0;
+
+  &:hover {
+       opacity: 1;
+        transition-duration: 1s;
+  }
+`;
+
+const SignOut = styled.div`
+   position: relative;
    height: 48px;
-   border-radius: 50%;
-   cursor: pointer
+   width: 48px;
+   display: flex;
+   cursor: pointer;
+   align-items: center;
+   justify-content: center;
+
+   ${UserImg} {
+       border-radius: 50%;
+       width: 100%;
+       heighT: 100%;
+   }
+
+    &:hover {
+      ${DropDown} {
+          opacity: 1;
+          transition-duration: 1s;
+      }
+  }
 `
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import ImgSlider from './ImgSlider'
 import Movies from './Movies'
 import Viewers from './Viewers'
+import db from '../firebase'
+import { collection, onSnapshot, query } from 'firebase/firestore'
+import { useDispatch } from 'react-redux'
+import { setMovies} from '../features/movie/movieSlice'
+
+import { Backdrop, CircularProgress } from '@material-ui/core'
+
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+
+
+  useEffect(() => {
+    onSnapshot(query(collection(db, 'movies')), (snapshot) => {
+      let tempMovies = snapshot.docs.map((doc) => {
+        return { id: doc.id, ...doc.data()}
+      })
+
+      dispatch(setMovies(tempMovies))
+      setLoading(false)
+    })
+  }, [])
 
   return (
     <Container>
       <ImgSlider />
       <Viewers />
-      <Movies />
+      { !loading 
+        ?  <Movies/> 
+        : <Backdrop
+            sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
+            open
+          >
+            <CircularProgress color="inherit" />
+          </Backdrop>
+      }
+     
     </Container>
   )
 }
