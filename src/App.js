dadae33db0bfd6a30a1ec3aba26a77c6e701523d
Marveeen <jmvroqueee@gmail.com>
2022-03-07 09:25:58 +0800

    Modified Redux Store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Detail from './components/Detail';
 import Header from './components/Header';
 import Home from './components/Home';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className='App'>
-      <Header />
-      <Home />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/home' element={<Home />} />
+          <Route path='/detail/:id' element={<Detail />} />
+          <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
